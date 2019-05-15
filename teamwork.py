983f3749665dcb2fce50d5e685396881e71511ec
phette23 <phette23@gmail.com>
2019-05-15 14:25:17 -0700

    capture cca email, more comments for teamwork.py

--- teamwork.py
+++ teamwork.py
@@ -3,9 +3,19 @@ import csv
 import re
 import sys
 
+# convert Teamwork "Agent" name into CCA email
+name_email_map = {
+    'Bobby White': 'bobbywhite',
+    'Ryan Segal': 'ryansegal',
+    'Sunny Satpathy': 'santrupti.satpathy',
+}
+
 
 def process_tags(tags):
     """ Look in Teamwork Desk "Tags" for info that fits into our other categories
+
+    Args: tags string
+    Returns: dict with patron_type, location, and details properties
     """
     # hash breaking out particular fields we can find in Teamwork tags
     d = {
@@ -15,16 +25,22 @@ def process_tags(tags):
         'details': '',
     }
     for tag in tags.split(','):
+        # NOTE: for both patron type & location the way I wrote this, if there
+        # are _multiple_ tags of either type then the _last one listed_ is the
+        # one that's used e.g. tags = "Staff,Faculty" -> patron_tpe = "Faculty"
+
         # look for patron types
-        ptypes = ['Undergrad', 'Faculty', 'Grad Student', 'Staff']
+        ptypes = ['Undergrad', 'Faculty', 'Grad Student', 'Staff', 'Alumni', 'Pre-college']
         for type in ptypes:
             if re.match(type, tag, re.IGNORECASE):
                 d['patron_type'] = type
+
         # look for locations
         locations = ['San Francisco', 'Oakland']
         for location in locations:
             if re.match(location, tag, re.IGNORECASE):
                 d['location'] = location
+
         # look for "details"
         details = [ 'Printing', 'Materials Library', 'VAULT',
             'Misguided Phone Call', 'Moodle', 'Archives Consultation',
@@ -54,7 +70,7 @@ def convert(tw):
     rs = []
 
     rs.append(tw['CreatedAt'])
-    rs.append('Teamwork Desk')
+    rs.append(name_email_map[tw['Agent']] + '@cca.edu')
     # @TODO - Directional/Reference/Service/Technical/Computing
     # probably only ever service or tech/comp
     rs.append('Service')
@@ -63,13 +79,14 @@ def convert(tw):
     tags = process_tags(tw['Tagged'])
     rs.append(tags['patron_type'])
     rs.append(tags['details'])
-    rs.append('interaction imported from Teamwork Desk')
+    rs.append('from Teamwork Desk')
+    # @TODO can we assume "online" for many of these?
     rs.append(tags['location'])
 
     return rs
 
 
-with open(sys.argv[1], 'rb') as infile:
+with open(sys.argv[1], 'r') as infile:
     with open('refstats.csv', 'w') as outfile:
         reader = csv.DictReader(infile)
         writer = csv.writer(outfile)
