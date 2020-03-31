34d8cd433147e914f134e9353ee2a0624eb9632f
phette23 <phette23@gmail.com>
2020-03-31 11:00:56 -0700

    add script for summing ACRL/IPEDS item types

---
+++ ipeds-item-types.js
@@ -0,0 +1,44 @@
+// https://library-staff.cca.edu/cgi-bin/koha/reports/guided_reports.pl?reports=172&phase=Run%20this%20report
+const map = {
+    '': 'Other',
+    'ARCHIVE': 'Books',
+    '2DAYRES': 'Books',
+    '2HOURRES': 'Books',
+    '3HOURRES': 'Books',
+    'LIBUSEBK': 'Books',
+    'BOOK': 'Books',
+
+    'DVD': 'Media',
+    'EQUIPMENT': 'Media',
+    'LIBUSEDVD': 'Media',
+    'LIBUSESUPP': 'Media',
+    'LIBUSEVIDE': 'Media',
+    'MATSAMP': 'Media',
+    'MIXEDMEDIA': 'Media',
+    'REALIA': 'Media',
+    'SUPPL': 'Media',
+    'VIDEOTAPE': 'Media',
+
+    'BOUNDPER': 'Serials',
+    'NEWPER': 'Serials',
+}
+let sum = {}
+// build the categories from the map
+for (let key in map) {
+    if (!sum[map[key]]) sum[map[key]] = 0
+}
+// loop over table & add each row to its matching sum entry
+$('table tr').each((index, row) => {
+    if ($(row).find('td').length === 2) {
+        let type = $(row).find('td').first().text()
+        let quantity = parseInt($(row).find('td').last().text())
+        sum[map[type]] += quantity
+    }
+})
+// printy/copy to clipboad in a nicer format
+let out = ''
+out += 'Type\tNumber of Titles\n'
+for (let key in sum) {
+    if (key !== 'undefined') out += `${key}\t${sum[key]}\n`
+}
+console.log(out)
