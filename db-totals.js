02079bbdf814ccaf8069a23a427c2ebd24804cbb
phette23 <phette23@gmail.com>
2022-03-21 19:52:34 -0700

    rewrite DB totals script, update with current collections
    use regex so the map entry doesn't have to have so many entries

--- db-totals.js
+++ db-totals.js
@@ -1,38 +1,36 @@
 // Serials Solutions Data Management page > Show All
 // https://clientcenter.serialssolutions.com/CC/Library/DataManagement/Default.aspx?LibraryCode=CC9&ShowAll=1
 const map = {
-    "Art and Architecture Source": "Digital/Electronic Serials",
+    "Art & Architecture Source": "Digital/Electronic Serials",
+    "ARTbibliographies Modern": "Digital/Electronic Serials",
     "ARTstor": "Digital/Electronic Media",
     "Avery Index to Architectural Periodicals": "Digital/Electronic Serials",
     "Bloomsbury Design Library Core Collection": "Digital/Electronic Books",
-    "Bloomsbury(Taylor & Francis):Full Collection:2015": "Digital/Electronic Serials",
+    "Bloomsbury.*Full Collection\:2015": "Digital/Electronic Serials",
     "California College of the Arts Libguides": "(ignored)",
     "California College of the Arts Library Catalog": "(ignored)",
     "CCA Library Catalog": "(ignored)",
     "CCA Print Holdings": "(ignored)",
-    "Design & Applied Arts Index (DAAI)": "Digital/Electronic Serials",
+    "Design & Applied Arts Index": "Digital/Electronic Serials",
     "Duke University Press": "Digital/Electronic Serials",
+    "Ebook Central - Business Ebook Subscription": "Digital/Electronic Books",
+    "e-Duke Books Scholarly Collection .*": "Digital/Electronic Books",
     "Encyclopedia Britannica Academic Edition": "Digital/Electronic Serials",
     "Gale Literature Resource Center": "Digital/Electronic Serials",
     "GreenFILE": "Digital/Electronic Serials",
     "JSTOR Archive Collection A-Z Listing": "Digital/Electronic Serials",
     "JSTOR Arts & Sciences III": "Digital/Electronic Serials",
     "Kanopy PDA - USA": "Digital/Electronic Media",
-    "Library, Information Science & Technology Abstracts (LISTA)": "Digital/Electronic Serials",
+    "Library, Information Science & Technology Abstracts": "Digital/Electronic Serials",
     "Material ConneXion": "Digital/Electronic Media",
     "MIT Press Journals": "Digital/Electronic Serials",
-    "MITPressDirect 2005 Archival Collection": "Digital/Electronic Books",
-    "MITPressDirect 2006 to 2010 Archival Collection": "Digital/Electronic Books",
-    "MITPressDirect 2011 to 2015 Archival Collection": "Digital/Electronic Books",
-    "MITPressDirect 2016 Collection": "Digital/Electronic Books",
-    "MITPressDirect 2017 Collection": "Digital/Electronic Books",
-    "MITPressDirect 2018 Collection": "Digital/Electronic Books",
-    "MITPressDirect 2019 Collection": "Digital/Electronic Books",
-    "MITPressDirect 2020 Collection": "Digital/Electronic Books",
+    "MIT Press Direct .* Collection": "Digital/Electronic Books",
+    "MITPressDirect .* Collection": "Digital/Electronic Books",
     "Newspaper Source": "Digital/Electronic Serials",
     "Oxford Art Online": "Digital/Electronic Books",
     "Oxford English Dictionary": "Digital/Electronic Books",
-    "Research Library": "Digital/Electronic Serials",
+    "ProQuest One Business": "(ignored)",   // see logged note at the end
+    "^Research Library$": "Digital/Electronic Serials", // need ^$ otherwise this matches "Open Research Library (Open Access)" too
     "Single Journals": "Digital/Electronic Serials",
     // the two titles we have from Springer are just OA books
     // "Springer Books": "Digital/Electronic Books",
@@ -40,7 +38,8 @@ const map = {
     "Underground and Independent Comics, Comix, and Graphic Novels: Volume 1": "Digital/Electronic Books",
     "University of California Press Journals": "Digital/Electronic Serials",
     "University of Chicago Press Journals (Current access)": "Digital/Electronic Serials",
-    "VAULT": "(ignored)"
+    "VAULT": "(ignored)",
+    "Wiley Online Library All Journals": "Digital/Electronic Serials"
 }
 
 let sum = { "Databases": 0, "Open Access": 0}
@@ -49,25 +48,40 @@ for (let key in map) {
     if (!sum[map[key]]) sum[map[key]] = 0
 }
 
+let keys = Object.keys(map)
+let patterns = keys.map(key => new RegExp(key))
+function addDBtoTotal(name, titles) {
+    let matched = false
+    for (let i = 0; i < keys.length; i++) {
+        if (name.match(patterns[i])) {
+            matched = true
+            let type = map[keys[i]]
+            console.log(`${name} is categorized as "${type}"`)
+            console.log('count', parseInt(titles.replace(/ of \d+/, '')))
+            sum[type] += parseInt(titles.replace(/ of \d+/, ''))
+            // track total of databases without including a) ebook collections
+            // & b) single journals under publishers like Duke U Press
+            if (!type.match('Books') && !titles.match(/ of /)) sum.Databases++
+            break
+        }
+    }
+    if (!matched) {
+        console.log(`Assuming ${name} is OA`)
+        sum["Open Access"] += parseInt(titles.replace(/ of \d+/, ''))
+    }
+}
+
 $('#ctl00__lvph_DatabaseListGrid tr').each((idx, row) => {
     let tds = $(row).find('td')
     if (!$(row).hasClass('GridViewHeader')) {
         let name = tds.eq(1).text().trim()
-        let quantity = parseInt(tds.eq(6).text().replace(/ of \d+/,''))
-        if (map.hasOwnProperty(name)) {
-            sum[map[name]] += quantity
-            // track total of databases without including a) ebook collections
-            // & b) single journals under publishers like Duke U Press
-            if (!map[name].match('Books') && !tds.eq(6).text().match(/ of /)) sum.Databases++
-        } else {
-            console.log(`Assuming "${name}" is OA`)
-            sum["Open Access"] += quantity
-        }
+        let titles = tds.eq(6).text().trim()
+        addDBtoTotal(name, titles)
     }
 })
 
-let out = 'Type\tNumber of Titles\n'
-for (let key in sum) {
-    if (key !== 'undefined') out += `${key}\t${sum[key]}\n`
-}
-console.log(out)
+console.log('Type\tNumber of Titles\n')
+Object.keys(sum).forEach(key => {
+    if (key) console.log(`${key}\t${sum[key]}\n`)
+})
+console.log("NOTE: ProQuest One Business contains both ebooks and journals and so is not included in these totals. Visit the database detail page then add its titles to the above.")
