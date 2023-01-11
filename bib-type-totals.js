ff8aa6fbcc9d9ff3389531e89e778651e5f67b8e
phette23 <phette23@gmail.com>
2023-01-10 17:06:02 -0800

    adjust bib/item type totals script, rm item type by branch code script

--- bib-type-totals.js
+++ bib-type-totals.js
@@ -8,6 +8,7 @@ const map = {
     'BK': 'Books',
     'BOOK': 'Books',
     'BOUNDPER': 'Periodicals',
+    'DIGITAL': 'Other',
     'DVD': 'Videos',
     'EBOOK': 'Digital/Electronic Books',
     'EQUIPMENT': 'Other',
@@ -20,19 +21,22 @@ const map = {
     'MIXEDMEDIA': 'Other',
     'NEWPER': 'Periodicals',
     'REALIA': 'Other',
-    'SUPPL': 'Other'
+    'SUPPL': 'Other',
+    'VIDEOTAPE': 'Videos',
 }
 let sum = {}
 // build the categories from the map
 for (let key in map) {
-    if (!sum[map[key]]) sum[map[key]] = 0
+    if (!sum[map[key]]) sum[map[key]] = {Titles: 0, Items: 0}
 }
 // loop over table & add each row to its matching sum entry
 $('table tr').each((index, row) => {
-    if ($(row).find('td').length === 2) {
-        let type = $(row).find('td').first().text()
-        let quantity = parseInt($(row).find('td').last().text())
-        sum[map[type]] += quantity
+    if ($(row).find('td').length === 3) {
+        let type = $(row).find('td').eq(0).text().trim()
+        let titles = parseInt($(row).find('td').eq(1).text().trim())
+        let items = parseInt($(row).find('td').eq(2).text().trim())
+        sum[map[type]].Titles += titles
+        sum[map[type]].Items += items
     }
 })
 // sort categories alphabetically but put 'Other' at the end
@@ -41,8 +45,16 @@ cats.splice(cats.indexOf('Other'), 1)
 cats.push('Other')
 
 // printy/copy to clipboad in a nicer format
-let out = 'Type\tNumber of Titles\n'
-cats.forEach(c => if (c !== 'undefined') out += `${c}\t${sum[c]}\n`)
+let out = 'Type\tNumber of Titles\tNumber of Items\n'
+cats.forEach(c => {
+    // handle edge case (true for Digital/Electronic) where we have more items
+    // than titles, which doesn't really make sense (it's because of item-less
+    // bib records that are just links to websites)
+    let items = sum[c].Items
+    let titles = sum[c].Titles
+    if (items < titles) items = titles
+    if (c !== 'undefined') out += `${c}\t${titles}\t${items}\n`
+})
 
 console.log(out)
 console.log("\n%c copied to your clipboard", "font-style: italic")
